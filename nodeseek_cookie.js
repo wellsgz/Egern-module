f12101fdcc26d63b971eb39f4dc60d2095451b78
wellsgz <wellsgz@hotmail.com>
2025-12-25 16:26:57 +0800

    Refactor NodeSeek cookie capture logic

--- nodeseek_cookie.js
+++ nodeseek_cookie.js
@@ -1,25 +1,19 @@
-// NodeSeek Cookie Capture for Egern
-const $ = new Env('NodeSeek');
+// NodeSeek Cookie Capture - Egern/Surge Compatible
+const cookieName = 'NodeSeek';
+const cookieKey = 'cookie_nodeseek';
 
-// Get cookie from request headers
-const cookieValue = $request.headers['Cookie'] || $request.headers['cookie'];
-
-if (cookieValue) {
-    // Save cookie
-    $.setdata(cookieValue, 'CookieNS');
-    $.msg('NodeSeek', 'Cookie获取成功 ✅', 'Cookie已保存，可以进行签到了');
-    console.log('NodeSeek Cookie saved: ' + cookieValue);
-} else {
-    $.msg('NodeSeek', 'Cookie获取失败 ❌', '未找到Cookie，请确保已登录');
+// Check if this is running in http-request context
+if (typeof $request !== 'undefined' && $request.headers) {
+    const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+    
+    if (cookie) {
+        $persistentStore.write(cookie, cookieKey);
+        $notification.post(cookieName, 'Cookie获取成功 ✅', 'Cookie已保存');
+        console.log('[NodeSeek] Cookie saved successfully');
+    } else {
+        $notification.post(cookieName, 'Cookie获取失败 ❌', '未找到Cookie');
+        console.log('[NodeSeek] No cookie found in request');
+    }
 }
 
-$.done({});
-
-// Env class for Egern
-function Env(name) {
-    this.name = name;
-    this.setdata = (val, key) => $persistentStore.write(val, key);
-    this.getdata = (key) => $persistentStore.read(key);
-    this.msg = (title, subtitle, message) => $notification.post(title, subtitle, message);
-    this.done = (obj = {}) => $done(obj);
-}
+$done({});
